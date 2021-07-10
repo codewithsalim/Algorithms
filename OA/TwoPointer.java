85e5b6ea95bf5243dd97d73dc6af6316d153dcac
Salim Shaikh <saleems756@gmail.com>
2021-07-11 02:35:36 +0530

    Update TwoPointer.java

--- OA/TwoPointer.java
+++ OA/TwoPointer.java
@@ -1,3 +1,16 @@
+/*
+Given 2 lists a and b. Each element is a pair of integers where the first integer represents the unique id and the second integer represents a value. Your task is to find an element from a and an element form b such that the sum of their values is less or equal to target and as close to target as possible. Return a list of ids of selected elements. If no pair is possible, return an empty list.
+
+Example 1:
+
+Input:
+a = [[1, 2], [2, 4], [3, 6]]
+b = [[1, 2]]
+target = 7
+
+Output: [[2, 1]]
+*/
+
 public List<int[]> findPairs(int[][] a, int[][] b, int target) {
         
     // a = [[1, 8], [3, 9 ], [2, 15]] <-- j
